5ee00f6db3bf4e30b70ab57ada5fe6952559dde7
RA <46924431+ratheraarif@users.noreply.github.com>
2026-09-08 13:23:20 -0400

    Detach alt_pred_r before flipping
    Detach alt_pred_r from the computation graph before flipping.

--- model/scripts/score_kidformer.py
+++ model/scripts/score_kidformer.py
@@ -90,7 +90,7 @@ def main():
                 ref_pred_r = torch.flip(ref_pred_r, dims=[1]).detach().cpu()  
                 ref_pred = (ref_pred_f + ref_pred_r) / 2
                 alt_pred_f = model(alt_onehot)
-                alt_pred_r = model(rev_comp(alt_onehot))
+                alt_pred_r = model(rev_comp(alt_onehot)).detach().cpu()  
                 alt_pred_r = torch.flip(alt_pred_r, dims=[1]).detach().cpu()  
                 alt_pred = (alt_pred_f + alt_pred_r) / 2
                 
